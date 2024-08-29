8b89f3637d29b6a7c50b132f9f53b309729713b2
thebeardedguru <3viserate@gmail.com>
2024-08-28 22:45:31 -0400

    init-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -18,9 +18,7 @@ export async function Navbar() {
     <nav className='relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7'>
       <div className='md:col-span-3'>
         <Link href='/'>
-          <h1 className='text-2xl font-semibold'>
-            Chatty<span className='text-primary'>&apos;</span>s
-          </h1>
+          <img src='/chattyscloset-logo_light.jpg' alt='ChattysCloset Logo' />
         </Link>
       </div>
 
